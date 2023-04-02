82ca8d96085bdc53559eeb0fa31c90e275f6d162
jberger2992 <jberger2992@gmail.com>
2023-04-02 17:00:29 -0400

    initial code

---
+++ script.js
@@ -0,0 +1,5 @@
+var questionH2 = document.querySelector("#question");
+var buttonA = document.querySelector("#button-a");
+var buttonB = document.querySelector("#button-b");
+var buttonC = document.querySelector("#button-c");
+var buttonD = document.querySelector("#button-d");
